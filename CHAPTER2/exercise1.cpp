0d8f34e9a1f863e9787f5916c58b78776202b08f
depiction <depiction@debian.com>
2024-08-05 00:57:59 -0400

    Second chapter started

---
+++ CHAPTER2/exercise1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+struct Point {
+    double x;
+    double y;
+};
+
+struct Vector {
+    double x;
+    double y;
+    double z;
+};
+
+
+Point A = {1, 2};
+Point B = {2, 3};
+Point C = {3, 4};
+
+double areaOfTriangle
