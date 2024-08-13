47f5ebc3d940b8513d36055858cb74d8a9f0acb6
depiction <depiction@debian.com>
2024-08-12 21:43:42 -0400

    5th point of the second chapter finished

--- CHAPTER2/exercise4.cpp
+++ CHAPTER2/exercise4.cpp
@@ -19,34 +19,40 @@ struct PolygonWithFiveVertices {
     }
 };
 
-string printsCentroidOfPolygonsWithFiveVertices(PolygonWithFiveVertices &polygon);
+void printsCentroidOfPolygonsWithFiveVertices(PolygonWithFiveVertices &polygon);
+double calculatesCentroidXOfPolygonsWithFiveVertices(PolygonWithFiveVertices &polygon);
+double calculatesCentroidYOfPolygonsWithFiveVertices(PolygonWithFiveVertices &polygon);
+double summation(PolygonWithFiveVertices &polygon, string axis);
+double calculatesFactors(Point current, Point next, string axis);
+double calculatesAreaOfPolygonWithFiveVertices(PolygonWithFiveVertices polygon);
 
-int main() {
-    printsCentroidOfPolygonsWithFiveVertices();
-    return 0;
-}
 
-string printsCentroidOfPolygonsWithFiveVertices(PolygonWithFiveVertices &polygon) {
+void printsCentroidOfPolygonsWithFiveVertices(PolygonWithFiveVertices &polygon) {
     double cx, cy;
     cx = calculatesCentroidXOfPolygonsWithFiveVertices(polygon);
     cy = calculatesCentroidYOfPolygonsWithFiveVertices(polygon);
-    cout << "The centroid of the polygon is (" + to_string(cx) + ", " + to_string(cy) + ")";
+    cout << "The centroid of the polygon is (" + to_string(cx) + ", " + to_string(cy) + ")" << endl;
 }
 
-double calculatesCentroidXOfPolygonsWithFiveVertices(PolygonWithFiveVertices polygon) {
-    double firstFactor, summation, x;
-    firstFactor = (1.0 / (6.0 * calculatesAreaOfPolygon(a));
+double calculatesCentroidXOfPolygonsWithFiveVertices(PolygonWithFiveVertices &polygon) {
+    return (1.0 / (6.0 * calculatesAreaOfPolygonWithFiveVertices(polygon))) * summation(polygon, "x");
 };
 
-double summation(PolygonWithFiveVertices polygon, string axis) {
+double calculatesCentroidYOfPolygonsWithFiveVertices(PolygonWithFiveVertices &polygon) {
+    return (1.0 / (6.0 * calculatesAreaOfPolygonWithFiveVertices(polygon))) * summation(polygon, "y");
+};
+
+double summation(PolygonWithFiveVertices &polygon, string axis) {
     std::vector<Point> vertices = polygon.getVerticesInOrder();
     int vertexCounter = 0;
     double factor1, factor2;
     double summation = 0;
     for (const auto& vertex : vertices) {
-        if (vertexCounter != 4) break;
+        if (vertexCounter == 4) break;
         summation +=  calculatesFactors(vertex, vertices.at(vertexCounter + 1), axis);
+        vertexCounter++;
     }
+    return summation;
 }
 
 double calculatesFactors(Point current, Point next, string axis) {
@@ -56,8 +62,27 @@ double calculatesFactors(Point current, Point next, string axis) {
     return factor1 * factor2;
 }
 
-double calculatesCentroidYOfPolygonsWithFiveVertices(PolygonWithFiveVertices polygon) {
+double calculatesAreaOfPolygonWithFiveVertices(PolygonWithFiveVertices polygon) {
+    polygon.getVerticesInOrder();
+    std::vector<Point> vertices = polygon.getVerticesInOrder();
+    double summation = 0;
+    double xi, yi, xi1, yi1;
+    for (int i = 0; i < 4; i++) {
+        xi = vertices.at(i).x;
+        xi1 = vertices.at(1+i).x;
+        yi = vertices.at(i).y;
+        yi1 = vertices.at(i+1).y;
+        summation += (xi*yi1 - yi*xi1);
+    }
+    return (1.0 / 2.0) * (summation);
+}
 
-};
+int main() {
+    PolygonWithFiveVertices polygon1 = {{0, 0}, {4, 0}, {4, 3}, {2, 5}, {0, 3}};
+    printsCentroidOfPolygonsWithFiveVertices(polygon1);
 
-double 
+    PolygonWithFiveVertices polygon2 = {{1, 2}, {6, 2}, {5, 5}, {3, 6}, {0, 4}};
+    printsCentroidOfPolygonsWithFiveVertices(polygon2);
+
+    return 0;
+}
